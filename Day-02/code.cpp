eaacb8692aeebc83a4c3f525b6a44264094c5f6f
Tirth S Pandya <teamfznltd@gmail.com>
2024-06-29 11:32:07 +0000

    commit

--- Day-02/code.cpp
+++ Day-02/code.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main(){
-    cout<<"Hello c++\n";
+    float PI1 = 3.14567890;
+    double PI2 = 3.14567890;
+    // cout<<"Hello c++\n";
+    // bool isAdult = true;
+    // int a = 54;
+    // int b = 43;
+    // cout<<a+b<<endl;
+    // cout<<"a = "<<a<<endl;
+    // cout<<"size of an int = "<<sizeof(int)<<endl;
+    // cout<<"I am an adult\n"<<isAdult<<endl;
+    // cout<<setprecision(12)<<"pi 1 = "<<PI1<<endl; garbage value print ho rhi
+    // cout<<setprecision(12)<<"pi 2 = "<<PI2<<endl; exact precision ke saath print krega
     return 0;
 }
