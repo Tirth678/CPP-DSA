f6b85dbc699ce70b220e3b92b8b6de2395efdf66
Tirth S Pandya <teamfznltd@gmail.com>
2024-06-30 07:36:10 +0000

    commit

---
+++ Day-04/3.cpp
@@ -0,0 +1,26 @@
+// income tax calculator
+#include<iostream>
+using namespace std;
+int main(){
+    int income;
+    cout<<"enter your income"<<endl;
+    cin>>income;
+    if(income<500000){
+        cout<<"tax applied 0%";
+    }
+
+    else if(income>50000 or income<1000000){
+        cout<<"tax applied 20% = "<<income*0.2<<endl;
+        
+    }
+    else if(income>1000000){
+        cout<<"tax applied 30% = "<<income*0.3<<endl;
+        return 0.30*income;
+    }
+    else{
+        cout<<" ";
+    }
+
+
+    return 0;
+}
---
+++ Day-04/4.cpp
@@ -0,0 +1,26 @@
+// print the largest number out of 3
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter the first value"<<endl;
+    cin>>a;
+    cout<<"enter the second value"<<endl;
+    cin>>b;
+    cout<<"enter the third value"<<endl;
+    cin>>c;
+    int max;
+    if(a>b){
+        max = a;
+    }
+    else if(b>c){
+        max = b;
+    }
+    else if(c>a){
+        max = c;
+    }   
+    cout<<"the maximun between the 3 is "<<max<<endl;
+
+
+    return 0;
+}
