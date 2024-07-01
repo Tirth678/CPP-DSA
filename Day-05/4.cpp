ced4bded6b92aea965b11584829011131e9cfae0
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-01 07:00:19 +0000

    commit

---
+++ Day-05/4.cpp
@@ -0,0 +1,23 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,n;
+//     // cout<<"enter the number of stars"<<endl;
+//     for (size_t i = 0; i < 5; i++)
+//     {
+//         cout<<"****"<<endl;
+//     }
+    
+
+//     return 0;
+// }
+
+// shraddha dis solution:
+#include<iostream>
+using namespace std;
+int main()[
+    
+
+
+    return 0;
+]
