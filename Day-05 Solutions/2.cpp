a2b8d49e3d71d9e85fe1c9a71501a4fd132a3c98
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-01 10:41:12 +0000

    commit

--- Day-05 Solutions/2.cpp
+++ Day-05 Solutions/2.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main(){
     int n,i;
-    // cout<<"enter the value"<<endl;
-    // cin>>n;
-    for (i=1;i<=10; i++)
+    cout<<"enter the value"<<endl;
+    cin>>n;
+    for (i=0;i<=10; i++)
     {
         // cout<<i<<endl;
-        cout<<i<<(5*(i+1))<<endl;
+        cout<<n<<"x"<<i<<"="<<(n*(i+0))<<endl;
     }
     
 
