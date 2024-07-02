7e3bd2c2cdeaaee46620e1d231f988b9e4362596
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-02 10:18:34 +0000

    commit

---
+++ Day-06/4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    char ch = 'A';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<< ch++;
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
