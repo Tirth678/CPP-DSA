4b729365a31f276f4e3648615fbe9613787d8ac5
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-04 10:29:50 +0000

    commit

--- Day-07 Solutions/1.cpp
+++ Day-07 Solutions/1.cpp
@@ -1,19 +1,6 @@
-// adding 2 numbers
 #include<iostream>
 using namespace std;
-int sum(int x, int y);
 int main(){
-    int a,b;
-    cout<<"enter the numbers to get the sum"<<endl;
-    cin>> a;
-    cin>>b;
-    cout<<"sum = "<<sum(a,b)<<endl;
-
 
     return 0;
-}
-int sum(int x, int y){
-    int result;
-    result = x+y;
-    return result;
 }
