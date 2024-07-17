8d31a55b23059c57dbce4dda6ed2cb5a00d02764
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-17 11:47:18 +0530

    commit

---
+++ Day-10/4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    int *ptr = &a;
+    int i;
+    cout<<ptr<<endl;
+    int arr[5] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(int);
+    // cout<<arr<<endl; // ek hexadecimal me value milegi
+    cout<<*arr<<endl;
+    cout<<*(arr+2)<<endl;
+    cout<<*(arr+3)<<endl;
+    cout<<*(arr+4)<<endl;
+    // agar arr[0] = 1000 hoga to value 1000 hi print hogi
+    // reason: pass by reference ho rha isiiye value change ho jayegi
+    
+
+
+    return 0;
+}
