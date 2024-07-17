6e71f43acaeb44e5e6abde76079a1054b310feb5
Tirth S Pandya <teamfznltd@gmail.com>
2024-07-17 11:11:28 +0530

    commit

---
+++ Day-10/1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int marks[50]; // 0 to 49
+    // int marks[50] = {1,2,3}; // 0 to 49
+    int marks[] = {1,2,3};
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<sizeof(marks)<<endl; // 12 aayega coz 4 byte=1 integer
+    // size of marks = 12/size of 1 int
+    // 12/4 = 3
+    // cout<<marks[3]<<endl; // random value print hogi
+    // cout<<marks[51]<<endl;
+    // a warning that 51th element does not exist so compiler just prints garbadge value
+
+    return 0;
+}
