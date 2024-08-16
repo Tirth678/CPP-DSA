2327e8c8be2792542813e201b248a890681257a6
Tirth S Pandya <teamfznltd@gmail.com>
2024-08-16 19:58:46 +0530

    commit

--- Day-10/3.cpp
+++ Day-10/3.cpp
@@ -1,25 +1,16 @@
-// find largest value in array;
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {5,4,3,9,12}; 
+    int arr[] = {2,3,4,65};
     int n = sizeof(arr)/sizeof(int);
     int max = arr[0];
-    int min = arr[0];
     for(int i=0; i<n; i++){
         if(arr[i]>max){
             max = arr[i];
-            cout<<arr[i]<<endl;
         }
-        if(arr[i]<min){
-            min = arr[i];
-            cout<<arr[i]<<endl;
-        }
+
     }
-    cout<<"max = "<<max<<endl;
-    cout<<"min = "<<min<<endl;
-
-
+    cout<<"largest value = "<<max<<endl;
 
     return 0;
 }
