f5d1783072c10025ec5b092e1b151d8e95161d3a
Tirth S Pandya <teamfznltd@gmail.com>
2024-11-28 11:46:42 +0530

    commit

--- OOPs/index.cpp
+++ OOPs/index.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
+#include<string>
 using namespace std;
+
+class Teacher{
+    // properties
+    string name;
+    string dept;
+    string subject;
+    double salary;
+
+    // methods(member functions)
+    void changedip(string newdept){
+        dept = newdept;
+    }
+};
 int main(){
+    Teacher t1,t2;
     
 
     return 0;
