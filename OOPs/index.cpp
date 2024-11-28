c9215dbf9718f03127d02102ebdc5b4a93799ff8
Tirth S Pandya <teamfznltd@gmail.com>
2024-11-28 11:58:41 +0530

    commit

--- OOPs/index.cpp
+++ OOPs/index.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 class Teacher{
+    // kisi property ko private bhi kr skte ho
+    private:
+    int age;
+    public:
     // properties
     string name;
     string dept;
@@ -15,8 +19,15 @@ class Teacher{
     }
 };
 int main(){
-    Teacher t1,t2;
-    
+    Teacher t1;
+    t1.name = "rekha";
+    t1.subject = "computer science and life science";
+    t1.dept = "BSc and MSc";
+    t1.salary = 32.32;
+    t1.age = 32; // error dikha rha coz age is private
+    cout<<"salary = "<<t1.salary<<endl;
+    cout<<"name = "<<t1.name<<endl;
+
 
     return 0;
 }
