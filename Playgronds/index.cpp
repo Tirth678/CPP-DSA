bd5f87a059c899544a4f7f6b37812b674644c8ba
Tirth S Pandya <teamfznltd@gmail.com>
2025-03-12 19:40:07 +0530

    practied some questions

--- Playgronds/index.cpp
+++ Playgronds/index.cpp
@@ -1,14 +1,18 @@
-// find largest element in an array
+// reverse an array with O(1) complexity
 #include<iostream>
 using namespace std;
 int main(){
-  int arr[10] = {1,2,3,4,5};
-  int max = arr[0];
-  for(int i=0; i<5; i++){
-      if(arr[i]>max){
-        max = arr[i];
-        return max;
-      }
+  int arr[] = {12,3,1,9};
+  int n = sizeof(arr)/sizeof(int);
+  int start = 0;
+  int end = n-1;
+  while(start<end){
+    swap(arr[start], arr[end]);
+    start++;
+    end--;
+  }
+  for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
   }
 
   return 0;
