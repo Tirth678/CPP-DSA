8ecb1e5c17fcfd5ebd3f80d425b0aa757950bc58
Tirth S Pandya <teamfznltd@gmail.com>
2025-03-17 11:19:01 +0530

    .

--- Basic sorting Algorithms/1.cpp
+++ Basic sorting Algorithms/1.cpp
@@ -14,6 +14,9 @@ int main(){
     int arr[5] = {5,4,3,1,2};
     int n = sizeof(arr)/sizeof(int);
     bubbleSort(arr, n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<endl;
+    }
 
     return 0;
 }
