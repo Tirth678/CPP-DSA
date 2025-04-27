079c8d85f345f8dc93f91e4b61138b30109e7ac9
Tirth S Pandya <teamfznltd@gmail.com>
2025-04-27 17:29:42 +0530

    .

--- Linked list/index.cpp
+++ Linked list/index.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
+class Node{
+    int data;
+    Node* next;
+public:
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+class LL{
+    Node* head;
+    Node* tail;
+};
 int main(){
-    cout<<"hi"<<endl;
     return 0;
 }
